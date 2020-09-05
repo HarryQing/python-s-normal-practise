ebc79bb5eacf253205cf1b2bdfaf21cf6615d16f
HarryQing <66860009+HarryQing@users.noreply.github.com>
2020-09-06 00:32:23 +0800

    字典学习
    字典学习

---
+++ 20.9.6  dictionary.py
@@ -0,0 +1,11 @@
+#创建一个字典并输出字典中所以元素
+
+information={
+    'first_name':'wang',
+    'last_name':'yong qing',
+    'age':'19',
+    'city':'nanchang',
+    }
+print(information)
+
+#结果{'first_name': 'wang', 'last_name': 'yong qing', 'age': '19', 'city': 'nanchang'}
