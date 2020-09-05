2adff12c013a88580305a39892c886034f89ad16
HarryQing <66860009+HarryQing@users.noreply.github.com>
2020-09-06 01:19:58 +0800

    学习字典
    学习字典

--- 20.9.5  dictionary.py
+++ 20.9.5  dictionary.py
@@ -37,3 +37,66 @@ print(information)
 print(information['first_name'].title())
 
 #结果Harry
+
+#遍历所以键—值对
+favorite_language={
+    'jen':'python',
+    'sarah':'c',
+    'tom':'ruby',
+    'lisa':'python',
+    }
+for key,value in favorite_language.items():
+    print("\nkey:"+key.title())
+    print("value:"+value)
+
+"""
+结果:
+key:Jen
+value:python
+
+key:Sarah
+value:c
+
+key:Tom
+value:ruby
+
+key:Lisa
+value:python
+"""
+
+#遍历字典中所以的键
+
+for key in favorite_language.keys():
+    print("\nkey:"+key.title())
+
+"""
+结果:
+key:Jen
+
+key:Sarah
+
+key:Tom
+
+key:Lisa
+"""
+friend=['tom']
+for key in favorite_language.keys():
+    print("\nkey:"+key.title())
+    if key in friend:
+        print("name:"+key+" love:"+favorite_language[key])
+
+"""
+结果:
+key:Jen
+
+key:Sarah
+
+key:Tom
+name:tom love:ruby
+
+key:Lisa
+"""
+if 'timi' not in favorite_language.keys():
+    print("nice")
+
+#结果:nice
